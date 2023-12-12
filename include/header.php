86f4a09b111befbd7535259ec54f3841b7abb92b
Daan <egdaan@gmail.com>
2023-12-12 14:15:26 +0100

    Fixed Navbar

--- include/header.php
+++ include/header.php
@@ -2,14 +2,14 @@
       <img
         src="Assets/images/Logo.png"
         alt=""
-        onclick="window.location='index.php'"
+        onclick="window.location='/?page=home'"
       />
       <div class="subHeader">
         <nav>
-          <a href="index.php" class="navItems">Home</a>
-          <a href="about.php" class="navItems">About</a>
-          <a href="projects.php" class="navItems">Projects</a>
-          <a href="contact.php" class="navItems">Contact</a>
+          <a href="/?page=home" class="navItems">Home</a>
+          <a href="/?page=about" class="navItems">About</a>
+          <a href="/?page=projects" class="navItems">Projects</a>
+          <a href="/?page=contact" class="navItems">Contact</a>
         </nav>
         <button class="hamburger">
           <div class="bar"></div>
